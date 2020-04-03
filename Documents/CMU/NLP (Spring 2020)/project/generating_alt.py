677b189be701a8212267a9d551b3e8590eabc00c
mmcqd <mmcquaid@andrew.cmu.edu>
2020-04-03 14:30:23 -0400

    added applyReplace to apply a replacement function to a list of string

--- Documents/CMU/NLP (Spring 2020)/project/generating_alt.py
+++ Documents/CMU/NLP (Spring 2020)/project/generating_alt.py
@@ -6,8 +6,8 @@ txt1 = "Autonomous cars shift insurance liability toward manufacturers"
 txt2 = "Pittsburgh is a city in the state of Pennsylvania in the United States, and is the county seat of Allegheny County."
 txt3 = "1999 is the year Matthew McQuaid was born"
 txt4 = "Matthew McQuaid was born in 1999"
-doc = nlp(txt4)
 
+txts = [nlp(x) for x in (txt1,txt2,txt3,txt4)]
 
 def curry2(f):
     return lambda x:lambda y:f(x,y)
@@ -32,6 +32,7 @@ def getLabel(word):
     return ents[0].label_ if len(ents) > 0 else None
 
 def searchChunks(deps,chunks):
+    entity = None
     for chunk in chunks:
         if chunk.root.dep_ in deps:
             entity = chunk.text
@@ -40,6 +41,7 @@ def searchChunks(deps,chunks):
     return (start,end,reps[getLabel(entity)]) if entity else None
 
 def searchToks(deps,doc):
+    entity = None
     for i,tok in enumerate(doc):
         if tok.dep_ in deps:
             entity = tok.text
@@ -68,4 +70,8 @@ obj  = ['dobj','iobj','pobj']
 replaceSubject = replace(subj)
 replaceObject = replace(obj)
 
+def applyReplace(rep,txts):
+    return [' '.join(x) for x in [rep(x) for x in txts] if x]
 
+print(applyReplace(replaceSubject,txts))
+print(applyReplace(replaceObject,txts))
